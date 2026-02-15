f01582aa76948b272732b509b885f00d02462bd1
Pothiraj <saravanapothi@gmail.com>
2026-02-16 01:42:26 +0530

    Update bank_account.py
    Added constructor to BankAccount class

--- bank-account-oop/bank_account.py
+++ bank-account-oop/bank_account.py
@@ -1,1 +1,7 @@
-Initial project structure
+# Bank Account OOP Project
+# Created by Saravanapothi
+
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
